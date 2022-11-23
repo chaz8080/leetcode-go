2afa3f96b6b75fae55bf6e8ee3bfb237ac35dc87
Charles Scholle <charles.scholle1@gmail.com>
2022-11-23 16:20:49 -0500

    Cleanup

--- 36-valid-sudoku/solution.go
+++ 36-valid-sudoku/solution.go
@@ -24,16 +24,12 @@ func isValidSudoku(board [][]byte) bool {
 			}
 
 			if _, found := rows[rowIndex][char]; found {
-				log.Println(rowIndex, colIndex)
-				log.Println(rows)
 				return false
 			} else {
 				rows[rowIndex][char] = true
 			}
 
 			if _, found := cols[colIndex][char]; found {
-				log.Println(rowIndex, colIndex)
-				log.Println(cols)
 				return false
 			} else {
 				cols[colIndex][char] = true
@@ -77,67 +73,5 @@ func getSubgroup(x, y int) int {
 	if x >= 6 && y >= 3 && y <= 5 {
 		return 7
 	}
-	if x >= 6 && y >= 6 && y <= 8 {
-		return 8
-	}
-
-	return -1
+	return 8
 }
-
-/**
-subgroups valid?
-row valid?
-cols valid?
-
-sg := []map{string}bool
-row := []map{string}bool
-cols := []map{string}bool
-
-5 -> 0,0
-	 x, y
-
-row[x].get(5) ? hit? exit false
-	row[x].set(5, true)
-
-col[y].get(5) ? hit? exit false
-	col[y].set(5, true)
-
-(x + 1) / 3
-(y + 1) / 3
-
-0 sg
-0,0
-0,1
-0,2
-1,0
-1,1
-1,2
-2,0
-2,1
-2,2
-
-if (x <= 2 && y <=2) {
-	sg = 0
-}
-
-1 sg
-0, 3
-0, 4
-0, 5
-1, 3
-1, 4
-1, 5
-2, 3
-2, 4
-2, 5
-
-if (x <= 2 && y >= 3 && y <=5) {
-	sg = 1
-}
-
-if (x <= 2 && y >= 6 && y <=8) {
-	sg = 2
-}
-
-
-*/
--- 36-valid-sudoku/solution_test.go
+++ 36-valid-sudoku/solution_test.go
@@ -29,11 +29,45 @@ func Test_isValidSudoku(t *testing.T) {
 			want: true,
 		},
 		{
-			name: "example 2",
+			name: "subgroup should fail",
 			args: args{
 				board: [][]byte{
 					[]byte("83..7...."),
 					[]byte("6..195..."),
+					[]byte(".98....6."),
+					[]byte("8...6...3"),
+					[]byte("4..8.3..1"),
+					[]byte("7...2...6"),
+					[]byte(".6....28."),
+					[]byte("...419..5"),
+					[]byte("....8..79"),
+				},
+			},
+			want: false,
+		},
+		{
+			name: "row should fail",
+			args: args{
+				board: [][]byte{
+					[]byte("838.7...."),
+					[]byte("6..195..."),
+					[]byte(".98....6."),
+					[]byte("8...6...3"),
+					[]byte("4..8.3..1"),
+					[]byte("7...2...6"),
+					[]byte(".6....28."),
+					[]byte("...419..5"),
+					[]byte("....8..79"),
+				},
+			},
+			want: false,
+		},
+		{
+			name: "column should fail",
+			args: args{
+				board: [][]byte{
+					[]byte("83..7...."),
+					[]byte("8..195..."),
 					[]byte(".98....6."),
 					[]byte("8...6...3"),
 					[]byte("4..8.3..1"),
