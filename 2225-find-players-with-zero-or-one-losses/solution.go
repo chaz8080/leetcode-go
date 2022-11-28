5d27a676dacd91df4f1f6a37411ccee8e3bd8f60
Charles Scholle <charles.scholle1@gmail.com>
2022-11-28 09:44:54 -0500

    Add 2225 - Find Players With Zero or One Losses

---
+++ 2225-find-players-with-zero-or-one-losses/solution.go
@@ -0,0 +1,40 @@
+package findplayerswithzerooronelosses
+
+import (
+	"sort"
+)
+
+func findWinners(matches [][]int) [][]int {
+	winners := map[int]int{}
+	losers := map[int]int{}
+
+	for _, m := range matches {
+		winner := m[0]
+		loser := m[1]
+
+		winners[winner] = winners[winner] + 1
+		losers[loser] = losers[loser] + 1
+	}
+
+	neverLost := []int{}
+	for w, _ := range winners {
+		if _, isAlsoLoser := losers[w]; !isAlsoLoser {
+			neverLost = append(neverLost, w)
+		}
+	}
+	sort.Slice(neverLost, func(i, j int) bool {
+		return neverLost[i] < neverLost[j]
+	})
+
+	lostOne := []int{}
+	for l, count := range losers {
+		if count == 1 {
+			lostOne = append(lostOne, l)
+		}
+	}
+	sort.Slice(lostOne, func(i, j int) bool {
+		return lostOne[i] < lostOne[j]
+	})
+
+	return [][]int{neverLost, lostOne}
+}
